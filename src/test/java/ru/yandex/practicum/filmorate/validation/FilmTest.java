704c1553e984da078d49baed60a3405bf0c3055b
Sofya Genzel <sofya.genzel@yandex.ru>
2022-10-31 21:10:39 +0300

    Sprint12 new version

--- src/test/java/ru/yandex/practicum/filmorate/validation/FilmTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/validation/FilmTest.java
@@ -15,6 +15,7 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.service.UserService;
 
+
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
